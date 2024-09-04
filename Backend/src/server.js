b1a71edcd1f69bec97306d210e6d3ea640e14ca5
Rodrigo Sandler <rodrigoqsandler@gmail.com>
2024-09-04 18:43:53 -0300

    added procfile

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,3 +5,13 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const path = require('path');
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../../Frontend/build')));
+
+// Serve the frontend's index.html for any unmatched routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Frontend/build', 'index.html'));
+});
